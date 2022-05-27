62543c89be4713917fc0eb5504daede3beeebe00
c1z <1660598843@qq.com>
2022-05-27 15:39:47 +0800

    second commit

--- src/main/java/com/example/gittest/gittest.java
+++ src/main/java/com/example/gittest/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println(223);
+        System.out.println("version 0.0.1");
     }
 }
