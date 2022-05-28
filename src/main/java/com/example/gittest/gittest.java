1cde036429a6b30708493a93f8c060b4461f2c49
c1z <1660598843@qq.com>
2022-05-28 08:04:02 +0800

    master commit

--- src/main/java/com/example/gittest/gittest.java
+++ src/main/java/com/example/gittest/gittest.java
@@ -6,5 +6,6 @@ public class gittest {
         System.out.println(223);
         System.out.println("version 0.0.1");
         System.out.println(1);
+        System.out.println("master commit");
     }
 }
